9f554ef2d6604451703b0624d9c8ba5a22a77f75
Cat <141940567+Aurivelle@users.noreply.github.com>
2024-12-22 11:42:35 +0800

    Brutal Force
    Time : O(QN)
    Space : O(1) Extra

--- Top Interview 150/2940. Find Building Where Alice and Bob can Meet.c
+++ Top Interview 150/2940. Find Building Where Alice and Bob can Meet.c
@@ -1,4 +1,4 @@
-/**
+ /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* leftmostBuildingQueries(int* heights, int heightsSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) 
