03648c0ebc0fafdf79fb1362f4be5a8c6197069d
Cat <141940567+Aurivelle@users.noreply.github.com>
2024-12-26 15:44:39 +0800

    DP
    Time : O(n * total)
    Space : O(n)

--- Daily Challenge/494. Target Sum.c
+++ Daily Challenge/494. Target Sum.c
@@ -1,24 +1,30 @@
-int dfs(int index, int currentsum, int target, int numsSize, int* nums)
-{
-    if(index == numsSize)
-    {
-        if(currentsum == target)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    int plus = dfs(index + 1, currentsum + nums[index], target, numsSize, nums);
-    int minus = dfs(index + 1, currentsum - nums[index], target, numsSize, nums);
-    return plus + minus;
 
-    
-}
 int findTargetSumWays(int* nums, int numsSize, int target) 
 {
-    int sum = dfs(0, 0, target, numsSize, nums);
+    int total = 0;
+    for(int i = 0; i < numsSize; i++)
+    {
+        total += nums[i];
+    }
+    if((total + target) % 2 || total < abs(target))
+    {
+        return 0;
+    }
+    total = (total + target) / 2;
+    int* dp = (int*)malloc(sizeof(int) * (total + 1));
+    for(int i = 0; i < total + 1; i++)
+    {
+        dp[i] = 0;
+    }
+    dp[0] = 1;
+    for(int i = 0; i < numsSize; i++)
+    {
+        for(int j = total; j >= nums[i]; j--)
+        {
+            dp[j] += dp[j - nums[i]];
+        }
+    }
+    int sum = dp[total];
+    free(dp);
     return sum;
 }
