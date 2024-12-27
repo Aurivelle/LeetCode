e89e7cda2ee3ca938735d99882989edbf5c51d16
Cat <141940567+Aurivelle@users.noreply.github.com>
2024-12-27 21:54:30 +0800

    Top Down DP
    Time : O(n^2)
    Space : O(1)

--- Top Interview 150/120. Triangle.c
+++ Top Interview 150/120. Triangle.c
@@ -1,15 +1,40 @@
-int dfs(int** triangle, int rnum, int row, int cnum, int column, int* columns)
+int min(int a, int b)
 {
-    if(rnum == row - 1)
-    {
-        return triangle[rnum][cnum];
-    }
-    int ret = dfs(triangle, rnum + 1, row, cnum, columns[rnum + 1], columns) < dfs(triangle, rnum + 1, row, cnum + 1, columns[rnum + 1], columns)?
-        dfs(triangle, rnum + 1, row, cnum, columns[rnum + 1], columns) : dfs(triangle, rnum + 1, row, cnum + 1, columns[rnum + 1], columns);
-    return ret + triangle[rnum][cnum];
+    return a < b ? a : b;
 }
 int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) 
 {
-    int min = dfs(triangle, 0, triangleSize, 0, triangleColSize[0], triangleColSize);
+    if(triangleSize == 1)
+    {
+        return triangle[0][0];
+    }
+    for(int i = 1; i < triangleSize; i++)
+    {
+        for(int j = 0; j < triangleColSize[i]; j++)
+        {
+            printf("%d ", triangle[i][j]);
+            if(j == 0)
+            {
+                triangle[i][j] += triangle[i - 1][j];
+            }
+            else if(j == triangleColSize[i] - 1)
+            {
+                triangle[i][j] += triangle[i - 1][j - 1];
+            }
+            else
+            {
+                triangle[i][j] += min(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+            printf("%d\n", triangle[i][j]);
+        }
+    }
+    int min = INT_MAX;
+    for(int i = 0; i < triangleColSize[triangleSize - 1]; i++)
+    {
+        if(triangle[triangleSize - 1][i] < min)
+        {
+            min = triangle[triangleSize - 1][i];
+        }
+    }
     return min;
 }
