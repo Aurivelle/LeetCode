a5b4e1f51bcdef85fc66a45083ed99098fb75b3f
Cat <141940567+Aurivelle@users.noreply.github.com>
2024-12-28 18:18:20 +0800

    Quickselect
    Time : Average O(n)
    Space : O(1)

--- Top Interview 150/215. Kth Largest Element in an Array.c
+++ Top Interview 150/215. Kth Largest Element in an Array.c
@@ -1,9 +1,43 @@
-int cmp(const void* a, const void* b)
+int partition(int* nums, int l, int r) 
 {
-    return - *(int*)a + *(int*)b;
+    int x = nums[r];
+    int i = l;
+    for (int j = l; j < r; j++) 
+    {
+        if (nums[j] <= x) 
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+        }
+    }
+    int temp = nums[i];
+    nums[i] = nums[r];
+    nums[r] = temp;
+    return i;
 }
+
 int findKthLargest(int* nums, int numsSize, int k) 
 {
-    qsort(nums, numsSize, sizeof(int), cmp);
-    return nums[k - 1];
+    int l = 0, r = numsSize - 1;
+    k = numsSize - k; 
+
+    while (l <= r) 
+    {
+        int index = partition(nums, l, r);
+        if (index == k)
+        {
+            return nums[index];
+        } 
+        else if (index < k) 
+        {
+            l = index + 1;
+        } 
+        else 
+        {
+            r = index - 1;
+        }
+    }
+    return INT_MAX;
 }
