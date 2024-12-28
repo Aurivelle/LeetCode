c122a446b3b568e6fbf03861abf3151413ed0321
Cat <141940567+Aurivelle@users.noreply.github.com>
2024-12-28 20:54:45 +0800

    Heap
    Time : O(nlogn)
    Space : O(n)

--- Top Interview 150/215. Kth Largest Element in an Array.c
+++ Top Interview 150/215. Kth Largest Element in an Array.c
@@ -1,43 +1,94 @@
-int partition(int* nums, int l, int r) 
+typedef struct
 {
-    int x = nums[r];
-    int i = l;
-    for (int j = l; j < r; j++) 
+    int* array;
+    int size;
+    int capacity;  
+} MaxHeap;
+
+MaxHeap* createHeap(int capacity)
+{
+    MaxHeap* heap = (MaxHeap*)malloc(sizeof(MaxHeap));
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->array = (int*)malloc(sizeof(int) * capacity);
+    return heap;
+}
+
+void siftUp(MaxHeap* heap, int index)
+{
+    int parent = (index - 1) / 2;
+
+    while(index > 0 && heap->array[index] > heap->array[parent])
     {
-        if (nums[j] <= x) 
-        {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-        }
+        int temp = heap->array[index];
+        heap->array[index] = heap->array[parent];
+        heap->array[parent] = temp;
+
+        index = parent;
+        parent = (index - 1) / 2;
     }
-    int temp = nums[i];
-    nums[i] = nums[r];
-    nums[r] = temp;
-    return i;
+}
+
+void insert(MaxHeap* heap, int value)
+{
+    if(heap->size == heap->capacity)
+    {
+        return;
+    }
+
+    heap->array[heap->size] = value;
+    heap->size++;
+    siftUp(heap, heap->size - 1);
+}
+void siftDown(MaxHeap* heap, int index)
+{
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    if(left < heap->size && heap->array[left] > heap->array[largest])
+    {
+        largest = left;
+    }
+    if(right < heap->size && heap->array[right] > heap->array[largest])
+    {
+        largest = right;
+    }
+    if(largest != index)
+    {
+        int temp = heap->array[largest];
+        heap->array[largest] = heap->array[index];
+        heap->array[index] = temp;
+
+        siftDown(heap, largest);
+    }
+}
+int extractMax(MaxHeap* heap)
+{
+    if(heap->size <= 0)
+    {
+        return INT_MAX;
+    }
+
+    int temp = heap->array[heap->size - 1];
+    heap->array[heap->size - 1] = heap->array[0];
+    heap->array[0] = temp;
+    heap->size--;
+
+    siftDown(heap, 0);
+    return heap->array[heap->size];
 }
 
 int findKthLargest(int* nums, int numsSize, int k) 
 {
-    int l = 0, r = numsSize - 1;
-    k = numsSize - k; 
+    MaxHeap* heap = createHeap(numsSize);
 
-    while (l <= r) 
+    for(int i = 0; i < numsSize; i++)
     {
-        int index = partition(nums, l, r);
-        if (index == k)
-        {
-            return nums[index];
-        } 
-        else if (index < k) 
-        {
-            l = index + 1;
-        } 
-        else 
-        {
-            r = index - 1;
-        }
+        insert(heap, nums[i]);
     }
-    return INT_MAX;
+    for(int i = 0; i < k - 1; i++)
+    {
+        extractMax(heap);
+    }
+    return extractMax(heap);
 }
