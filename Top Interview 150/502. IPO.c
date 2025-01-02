944690c34b063d90ca4e83eb9733675381651c98
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-01-03 02:23:01 +0800

    Heap
    Time : O(nlogn)
    Space : O(n)

--- Top Interview 150/502. IPO.c
+++ Top Interview 150/502. IPO.c
@@ -1,29 +1,128 @@
+typedef struct
+{
+    int profit;
+    int capital;
+} Project;
+
+int cmp(const void* a, const void* b)
+{
+    return ((Project*)a)->capital - ((Project*)b)->capital;
+}
+
+typedef struct
+{
+    int size;
+    int capacity;
+    int* profits;
+}Heap;
+
+Heap* createHeap(int capacity)
+{
+    Heap* heap = (Heap*)malloc(sizeof(Heap));
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->profits = (int*)malloc(sizeof(int) * capacity);
+    return heap;
+}
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void siftUp(Heap* heap, int index)
+{
+    if(index == 0)
+    {
+        return;
+    }
+    int parent = (index - 1) / 2;
+    if(heap->profits[parent] < heap->profits[index])
+    {
+        swap(&heap->profits[parent], &heap->profits[index]);
+    }
+    siftUp(heap, parent);
+}
+
+void insert(Heap* heap, int profit)
+{
+    if(heap->size == heap->capacity)
+    {
+        return;
+    }
+    heap->profits[heap->size] = profit;
+    heap->size++;
+    siftUp(heap, heap->size - 1);
+}
+
+void siftDown(Heap* heap, int index)
+{
+    if(index >= heap->size)
+    {
+        return;
+    }
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int smallest = index;
+    if(left < heap->size && heap->profits[smallest] < heap->profits[left])
+    {
+        smallest = left;
+    }
+    if(right < heap->size && heap->profits[smallest] < heap->profits[right])
+    {
+        smallest = right;
+    }
+    if(smallest != index)
+    {
+        swap(&heap->profits[smallest], &heap->profits[index]);
+        siftDown(heap, smallest);
+    }
+    
+}
+
+int extractMax(Heap* heap)
+{
+    int ret = heap->profits[0];
+    swap(&heap->profits[0], &heap->profits[heap->size - 1]);
+    heap->size--;
+    siftDown(heap, 0);
+    return ret;
+}
+
 int findMaximizedCapital(int k, int w, int* profits, int profitsSize, int* capital, int capitalSize) 
 {
-    int n = profitsSize;
-    int max;
-    int maxidx;
-    for(int i = 0; i < k; i++)
+    Project* project = (Project*)malloc(sizeof(Project) * profitsSize);
+    for(int i = 0; i < profitsSize; i++)
     {
-        max = -1;
-        maxidx = -1;
-        for(int j = 0; j < n; j++)
+        project[i].profit = profits[i];
+        project[i].capital = capital[i];
+    }
+
+    qsort(project, profitsSize, sizeof(Project), cmp);
+    Heap* heap = createHeap(profitsSize);
+
+    int i = 0;
+    for(int start = 0; start < k; start++)
+    {
+        while(i < profitsSize && project[i].capital <= w)
         {
-            if(w >= capital[j])
-            {
-                if(profits[j] > max)
-                {
-                    max = profits[j];
-                    maxidx = j;
-                }
-            }      
+            insert(heap, project[i].profit);
+            i++;
         }
-        if(max == -1)
+        if(heap->size == 0)
         {
             break;
         }
+        int max = extractMax(heap);
         w += max;
-        profits[maxidx] = -1;
     }
+    free(heap->profits);
+    free(heap);
+    free(project);
     return w;
+
+
+
 }
