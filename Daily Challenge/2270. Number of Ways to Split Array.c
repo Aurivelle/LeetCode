fa1e7ce55f621937d328e5a2e543dda4bf96a45e
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-01-03 12:08:47 +0800

    Optimized

--- Daily Challenge/2270. Number of Ways to Split Array.c
+++ Daily Challenge/2270. Number of Ways to Split Array.c
@@ -1,31 +1,18 @@
 int waysToSplitArray(int* nums, int numsSize) 
 {
-    if(numsSize == 1 || (numsSize == 2 && nums[0] < nums[1]))
-    {
-        return 0;
-    }
-    if(numsSize == 2 && nums[1] <= nums[0])
-    {
-        return 1;
-    }
     long long left[numsSize];
-    long long right[numsSize];
+    long long sum = nums[0];
     left[0] = 0;
-    right[numsSize - 1] = nums[numsSize - 1];
     for(int i = 1; i < numsSize; i++)
     {
+        sum += nums[i];
         left[i] = left[i - 1] + (long long)nums[i - 1];
     }
-    long long sum[numsSize];
     
-    for(int i = numsSize - 2; i >= 0; i--)
-    {
-        right[i] = right[i + 1] + (long long)nums[i];
-    }
     int count = 0;
     for(int i = 1; i < numsSize; i++)
     {
-        if(left[i] >= right[i])
+        if(left[i] >= sum - left[i])
         {
             count++;
         }
