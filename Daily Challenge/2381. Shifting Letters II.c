d50e60f687ab1e9e633199c3f81fc748367fadb0
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-01-05 17:37:55 +0800

    Difference Array
    Time : O(n + q)
    Space : O(n)

--- Daily Challenge/2381. Shifting Letters II.c
+++ Daily Challenge/2381. Shifting Letters II.c
@@ -1,44 +1,37 @@
-char forward(char s)
-{
-    if(s == 'z')
-    {
-        s = 'a';
-    }
-    else
-    {
-        s++;
-    }
-    return s;
-}
-
-char backward(char s)
-{
-    if(s == 'a')
-    {
-        s = 'z';
-    }
-    else
-    {
-        s--;
-    }
-    return s;
-}
-
 char* shiftingLetters(char* s, int** shifts, int shiftsSize, int* shiftsColSize) 
 {
+    int len = strlen(s);
+    int diff[len + 1];
+    for(int i = 0; i <= len; i++)
+    {
+        diff[i] = 0;
+    }    
     for(int i = 0; i < shiftsSize; i++)
     {
-        for(int j = shifts[i][0]; j <= shifts[i][1]; j++)
+        if(shifts[i][2])
         {
-            if(shifts[i][2])
-            {
-                s[j] = forward(s[j]);
-            }
-            else
-            {
-                s[j] = backward(s[j]);
-            }
+            diff[shifts[i][0]]++;
+            diff[shifts[i][1] + 1]--;
         }
-    }    
+        else
+        {
+            diff[shifts[i][0]]--;
+            diff[shifts[i][1] + 1]++;
+        }
+    }
+    for(int i = 1; i <= len; i++)
+    {
+        
+        diff[i] += diff[i - 1];
+    }
+    for(int i = 0; i < len; i++)
+    {
+        printf("%d\n", diff[i]);
+    }
+    for(int i = 0; i < len; i++)
+    {
+        s[i] = (char)(((s[i] - 'a' + diff[i]) % 26 + 26) % 26 + 'a');
+        
+    }
     return s;
 }
