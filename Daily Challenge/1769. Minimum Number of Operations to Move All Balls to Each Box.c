05b50b350127fec7f942e4be177f581634043111
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-01-06 09:38:55 +0800

    Prefix Sum
    Time : O(n)
    Space : O(n)

--- Daily Challenge/1769. Minimum Number of Operations to Move All Balls to Each Box.c
+++ Daily Challenge/1769. Minimum Number of Operations to Move All Balls to Each Box.c
@@ -4,22 +4,41 @@
 int* minOperations(char* boxes, int* returnSize)
 {
     int n = strlen(boxes);
-    
+    int count = 0;
+    int left[n];
+    left[0] = 0;
+    if(boxes[0] == '1')
+    {
+        count++;
+    }
+    for(int i = 1; i < n; i++)
+    {
+        left[i] = left[i - 1] + count;
+        if(boxes[i] == '1')
+        {
+            count++;
+        }
+    }
+    count = 0;
+    int right[n];
+    right[n - 1] = 0;
+    if(boxes[n - 1] == '1')
+    {
+        count++;
+    }
+    for(int i = n - 2; i >= 0; i--)
+    {
+        right[i] = right[i + 1] + count;
+        if(boxes[i] == '1')
+        {
+            count++;
+        }
+    }
     int* answer = (int*)malloc(sizeof(int) * n);
-    *returnSize = 0;
     for(int i = 0; i < n; i++)
     {
-        answer[i] = 0;
-        int count = 0;
-        for(int j = 0; j < n; j++)
-        {
-            if(boxes[j] == '1')
-            {
-                count += abs(j - i);
-            }
-        }
-        answer[*returnSize] = count;
-        (*returnSize)++;
+        answer[i] = left[i] + right[i];
     }
-    return answer;  
+    *returnSize = n;
+    return answer;
 }
