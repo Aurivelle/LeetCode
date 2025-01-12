053657fe7abe748ef7219ea6d49e5de136ab9839
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-01-13 00:16:10 +0800

    Bidirectional BFS
    Time : O(n^2)
    Space : O(n^2)

--- BFS/1091. Shortest Path in Binary Matrix.c
+++ BFS/1091. Shortest Path in Binary Matrix.c
@@ -13,31 +13,14 @@ Node* createNode(int r, int c)
     return node;
 }
 
-
-void enqueue(Node** queue, Node* node, int* size, int capacity, int* rear)
+bool isValid(int r, int c, int gridSize, int** grid, int** visited)
 {
-    if(*size == capacity)
-    {
-        return;
-    }
-    printf("%d %d\n", node->r, node->c);
-    queue[(*rear)++] = node;
-    (*size)++;
+    return r >= 0 && r < gridSize && c >= 0 && c < gridSize && grid[r][c] == 0 && !visited[r][c];
 }
 
-Node* dequeue(Node** queue, int* size, int capacity, int* front)
-{
-    if(*size == 0)
-    {
-        return NULL;
-    }
-    Node* node = queue[(*front)++];
-    (*size)--;
-    return node;
-}
 int shortestPathBinaryMatrix(int** grid, int gridSize, int* gridColSize) 
 {
-    if(grid[0][0] == 1)
+    if(grid[0][0] == 1 || grid[gridSize - 1][gridSize - 1] == 1)
     {
         return -1;
     }
@@ -45,54 +28,82 @@ int shortestPathBinaryMatrix(int** grid, int gridSize, int* gridColSize)
     {
         return 1;
     }
-    Node** queue = (Node**)malloc(sizeof(Node*) * gridSize * gridSize);
-    int capacity = gridSize * gridSize;
-    int size = 0;
-    int front = 0;
-    int rear = 0;
-    int count = 1;
-    int** visited = (int**)malloc(sizeof(int*) * gridSize);
+
+    int directions[8][2] = {{1, 1}, {1, 0}, {1, -1}, {0, 1}, {0, -1}, {-1, 1}, {-1, 0}, {-1, -1}};
+
+    int** visited1 = (int**)malloc(sizeof(int*) * gridSize);
+    int** visited2 = (int**)malloc(sizeof(int*) * gridSize);
     for(int i = 0; i < gridSize; i++)
     {
-        visited[i] = (int*)malloc(sizeof(int) * gridSize);
-        for(int j = 0; j < gridSize; j++)
-        {
-            visited[i][j] = 0;
-        }
+        visited1[i] = (int*)calloc(gridSize, sizeof(int));
+        visited2[i] = (int*)calloc(gridSize, sizeof(int));
     }
-    Node* start = createNode(0, 0);
-    visited[0][0] = 1;
-    enqueue(queue, start, &size, capacity, &rear);
-    while(front < rear)
-    {
-        int sizes = rear - front;
-        count++;
-        int directions[2][8] = {{1, 0, -1, 0, 1, 1, -1, -1}, {0, 1, 0, -1, 1, -1, 1, -1}};
-        for(int i = 0; i < sizes; i++)
-        {
-            Node* node = dequeue(queue, &size, capacity, &front);
 
+    Node** queue1 = (Node**)malloc(sizeof(Node*) * gridSize * gridSize);
+    int size1 = 0;
+    int front1 = 0;
+    int rear1 = 0;
+
+    Node** queue2 = (Node**)malloc(sizeof(Node*) * gridSize * gridSize);
+    int size2 = 0;
+    int front2 = 0;
+    int rear2 = 0;
+    
+    Node* start = createNode(0,0);
+    Node* end = createNode(gridSize - 1, gridSize - 1);
+    queue1[rear1++] = start;
+    queue2[rear2++] = end;
+    visited1[0][0] = 1;
+    visited2[gridSize - 1][gridSize - 1] = 1;
+    int steps = 1;
+    while(front1 < rear1 && front2 < rear2)
+    {
+        if(rear1 - front1 > rear2 - front2)
+        {
+            Node** tempQueue = queue1;
+            queue1 = queue2;
+            queue2 = tempQueue;
+
+            int tempsize = size1;
+            size1 = size2;
+            size2 = tempsize;
+
+            int tempfront = front1;
+            front1 = front2;
+            front2 = tempfront;
+
+            int temprear = rear1;
+            rear1 = rear2;
+            rear2 = temprear;
+
+            int** tempvisited = visited1;
+            visited1 = visited2;
+            visited2 = tempvisited;
+        }
+        int currentsize = rear1 - front1;
+        for(int i = 0; i < currentsize; i++)
+        {
+            Node* node = queue1[front1++];
             int r = node->r;
             int c = node->c;
             free(node);
+
             for(int j = 0; j < 8; j++)
             {
-                int newr = r + directions[0][j];
-                int newc = c + directions[1][j];
-                
-                if(newr < 0 || newr >= gridSize || newc < 0 || newc >= gridSize || visited[newr][newc] || grid[newr][newc])
+                int newr = r + directions[j][0];
+                int newc = c + directions[j][1];
+                if(isValid(newr, newc, gridSize, grid, visited1))
                 {
-                    continue;
+                    if(visited2[newr][newc])
+                    {
+                        return steps + 1;
+                    }
+                    visited1[newr][newc] = 1;
+                    queue1[rear1++] = createNode(newr, newc);
                 }
-                if(newr == gridSize - 1 && newc == gridSize - 1)
-                {
-                    return count;
-                }
-                Node* newNode = createNode(newr, newc);
-                visited[newr][newc] = 1;
-                enqueue(queue, newNode, &size, capacity, &rear);
             }
         }
+        steps++;
     }   
     
     return -1;
