555419f55e72831740e2d957b08073cdfe4595c6
Cat <141940567+Aurivelle@users.noreply.github.com>
2025-02-06 15:11:37 +0800

    Linked List
    Time : O(n^2)
    Space : O(n^2)

--- Daily Challenge/1726. Tuple with Same Product.c
+++ Daily Challenge/1726. Tuple with Same Product.c
@@ -1,34 +1,74 @@
-int compare(const void *a, const void *b) 
+
+typedef struct HashNode 
 {
-    return (*(int*)a - *(int*)b);
+    int key;        
+    int count;     
+    struct HashNode* next;
+} HashNode;
+
+
+int hashFunction(int key, int size) 
+{
+    return key % size;
 }
-int tupleSameProduct(int* nums, int numsSize) 
+
+
+void insert(HashNode** table, int key, int size) 
 {
-    qsort(nums, numsSize, sizeof(int), compare);
-    int maxProduct = nums[numsSize - 1] * nums[numsSize - 2];
-    int* hash = (int*)calloc(maxProduct + 1, sizeof(int));
-    
-    for (int i = 0; i < numsSize; i++) 
-    {
-        for (int j = i + 1; j < numsSize; j++) 
+    int index = hashFunction(key, size);
+    HashNode* node = table[index];
+
+    while (node) {
+        if (node->key == key) 
+        {
+            node->count++;
+            return;
+        }
+        node = node->next;
+    }
+
+    HashNode* newNode = (HashNode*)malloc(sizeof(HashNode));
+    newNode->key = key;
+    newNode->count = 1;
+    newNode->next = table[index];
+    table[index] = newNode;
+}
+
+int tupleSameProduct(int* nums, int numsSize) {
+    int hashSize = 100003; 
+    HashNode** hashTable = (HashNode**)calloc(hashSize, sizeof(HashNode*));
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++)
         {
             int product = nums[i] * nums[j];
-            hash[product]++;
+            insert(hashTable, product, hashSize);
         }
     }
 
-    int count = 0;
-   
-    for (int i = 0; i <= maxProduct; i++) 
+    int result = 0;
+    for (int i = 0; i < hashSize; i++)
     {
-        if (hash[i] >= 2) 
-        {
-            count += (hash[i] * (hash[i] - 1) / 2) * 8;
+        HashNode* node = hashTable[i];
+        while (node) {
+            if (node->count >= 2) 
+            {
+                result += (node->count * (node->count - 1) / 2) * 8;
+            }
+            node = node->next;
         }
     }
-    
 
-    free(hash);
-    
-    return count;
+    for (int i = 0; i < hashSize; i++) 
+    {
+        HashNode* node = hashTable[i];
+        while (node) {
+            HashNode* temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+    free(hashTable);
+
+    return result;
 }
